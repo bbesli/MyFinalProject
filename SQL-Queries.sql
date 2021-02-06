03f00cf016caf7be783e5f44a6b206b70a60963f
Burak Beşli <burak.besli@outlook.com>
2021-02-06 23:41:22 +0300

    06.02.2021- 2. Mola

---
+++ SQL-Queries.sql
@@ -0,0 +1,33 @@
+﻿Create Table Cars(
+CarId int,
+BrandId int,
+ColorId int,
+CarModel nvarchar,
+CarManufacturedYear Date,
+Primary Key (CarId)
+)
+
+
+Create Table Brands(
+BrandId int,
+BrandName nvarchar,
+Primary Key (BrandId)
+)
+
+Create Table Colors(
+ColorId int,
+ColorName nvarchar,
+Primary Key (ColorId)
+)
+
+ALTER TABLE Brands
+ALTER COLUMN BrandName varchar(30);
+
+ALTER TABLE Colors
+ALTER COLUMN ColorName varchar(20);
+
+ALTER TABLE Cars
+ALTER COLUMN CarModel varchar(20);
+
+
+

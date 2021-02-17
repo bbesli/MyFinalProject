c9b462504755aef21c6f5b2477df534a1137b5e8
Burak Beşli <burak.besli@outlook.com>
2021-02-18 00:11:46 +0300

    ders sonu something like hayatın sonu (zordu)

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -23,12 +24,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             //Magic strings kullanmamalıyız. Dolayısıyla Messages adında static bir class oluşturduk.
             //Validation
-
-            ValidationTool.Validate(new ProductValidator(), product);
 
             _productDal.Add(product);
 
